fe1c4e0aa9b16a835e03d506c3e9f6bcec71d2e1
CometMobile <cometmobileinfo@gmail.com>
2020-03-09 11:01:22 -0400

    Delete emailto.php

--- emailto.php
+++
@@ -1,7 +0,0 @@
-<?php     
-$to_email = 'cometsquaredemployees@gmail.com';
-$subject = 'Testing PHP Mail';
-$message = 'This mail is sent using the PHP mail function';
-$headers = 'From: norrisnt@gmail.com';
-mail($to_email,$subject,$message,$headers);
-?>
